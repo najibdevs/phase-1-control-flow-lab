af5dd08b0771b553af4c5dfaf1176b4b9b2fc5b4
najibdevs <najibsoftwareeng@gmail.com>
2024-03-20 09:41:17 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let greeting;
+  if (distance <= 400) {
+      greeting = "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+      greeting = "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+      greeting = "I will gladly take your thirty bucks.";
+  } else {
+      greeting = "No can do.";
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case "generous":
+          return "Thank you so much.";
+      case "not as generous":
+          return "Thank you.";
+      default:
+          return "Bye.";
+  }
 }
+
